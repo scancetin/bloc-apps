5eb4930bb94d81414f43318d16d9131461e5b03b
scancetin <scancetinbas@gmail.com>
2023-09-04 20:55:28 +0300

    cubit_travel_app initial commit

---
+++ cubit_travel_app/android/app/src/main/kotlin/com/example/cubit_travel_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cubit_travel_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ cubit_travel_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:cubit_travel_app/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: HomeView(),
+    );
+  }
+}
