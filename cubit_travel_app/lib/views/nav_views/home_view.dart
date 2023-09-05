53192956c22b6d4c6668308c11de21dffea8bc12
scancetin <scancetinbas@gmail.com>
2023-09-06 00:55:39 +0300

    home view created

--- cubit_travel_app/lib/views/nav_views/home_view.dart
+++ cubit_travel_app/lib/views/nav_views/home_view.dart
@@ -1,18 +1,29 @@
-// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
+import 'package:cubit_travel_app/widgets/custom_large_text.dart';
+import 'package:cubit_travel_app/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
+    TabController tabCon = TabController(length: 3, vsync: this);
+
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.all(20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 70),
+              padding: EdgeInsets.only(top: 50),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -32,6 +43,54 @@ class HomeView extends StatelessWidget {
                   )
                 ],
               ),
+            ),
+            SizedBox(height: 20),
+            Container(child: LargeCustomText(text: "Discover")),
+            SizedBox(height: 20),
+            Container(
+              child: TabBar(
+                labelColor: Colors.black,
+                controller: tabCon,
+                indicatorColor: Colors.black,
+                indicatorPadding: EdgeInsets.symmetric(horizontal: 57),
+                tabs: [
+                  Tab(text: "Places"),
+                  Tab(text: "Inspiration"),
+                  Tab(text: "Emotions"),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 300,
+              child: TabBarView(
+                controller: tabCon,
+                children: [
+                  ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.all(2),
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurpleAccent,
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                LargeCustomText(text: "Explore More", size: 22),
+                CustomText(text: "See all", color: Colors.purple),
+              ],
             )
           ],
         ),
