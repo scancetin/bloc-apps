97a9755a18916fa6038b97307efc82035b996466
scancetin <scancetinbas@gmail.com>
2023-09-21 02:28:21 +0300

    daily_news page created

---
+++ clean_arch_news_app/lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,50 @@
+import 'package:clean_arch_news_app/features/daily_news/presentation/bloc/article/remote/bloc/remote_article_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: const Text(
+        "Daily News",
+        style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  BlocBuilder<RemoteArticleBloc, RemoteArticleState> _buildBody() {
+    return BlocBuilder<RemoteArticleBloc, RemoteArticleState>(
+      builder: (_, state) {
+        if (state is RemoteArticleLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (state is RemoteArticleError) {
+          print(state.error!.message);
+          return const Center(child: Icon(Icons.refresh));
+        }
+        if (state is RemoteArticleDone) {
+          return ListView.builder(
+            itemCount: state.articles!.length,
+            itemBuilder: ((context, index) {
+              return ListTile(
+                title: Text(index.toString()),
+              );
+            }),
+          );
+        } else {
+          return Container();
+        }
+      },
+    );
+  }
+}
--- clean_arch_news_app/lib/main.dart
+++ clean_arch_news_app/lib/main.dart
@@ -1,21 +1,27 @@
+import 'package:clean_arch_news_app/config/theme/app_themes.dart';
+import 'package:clean_arch_news_app/features/daily_news/presentation/bloc/article/remote/bloc/remote_article_bloc.dart';
+import 'package:clean_arch_news_app/features/daily_news/presentation/pages/home/daily_news.dart';
+import 'package:clean_arch_news_app/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  await initializeDependencies();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return BlocProvider<RemoteArticleBloc>(
+      create: (context) => sl()..add(const GetArticles()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: theme(),
+        home: const DailyNews(),
       ),
     );
   }
