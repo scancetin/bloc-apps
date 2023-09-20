537f86dc8d8987d4b0fc8b558be9795423c9ed61
scancetin <scancetinbas@gmail.com>
2023-09-21 00:26:50 +0300

    usecases added

--- clean_arch_news_app/lib/core/constants/constants.dart
+++ clean_arch_news_app/lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseUrl = "http://newsapi.org/v2";
+const String newsAPIKey = "5dcdb5d4d30745bf9fcdd48aa2097bd0";
+const String countryQuery = "us";
+const String categoryQuery = "general";
--- clean_arch_news_app/lib/features/daily_news/data/repositories/article_repository.dart
+++ clean_arch_news_app/lib/features/daily_news/data/repositories/article_repository.dart
@@ -1,10 +1,37 @@
+import 'dart:io';
+
+import 'package:clean_arch_news_app/core/constants/constants.dart';
 import 'package:clean_arch_news_app/core/resources/data_state.dart';
+import 'package:clean_arch_news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:clean_arch_news_app/features/daily_news/data/models/models.dart';
+import 'package:dio/dio.dart';
 import '../../domain/repositories/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+  ArticleRepositoryImpl(this._newsApiService);
+
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticle() {
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticle() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsAPIKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(DioException(
+          error: httpResponse.response.statusCode,
+          response: httpResponse.response,
+          type: DioExceptionType.badResponse,
+          requestOptions: httpResponse.response.requestOptions,
+        ));
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
